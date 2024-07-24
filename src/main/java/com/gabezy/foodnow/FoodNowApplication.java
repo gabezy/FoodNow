dc251de9f3d370abac29c08c1eaeb72fbf5f166f
gabezy <gabrielmoreira2705@gmail.com>
2024-07-24 08:44:23 -0300

    first commit

---
+++ src/main/java/com/gabezy/foodnow/FoodNowApplication.java
@@ -0,0 +1,13 @@
+package com.gabezy.foodnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodNowApplication.class, args);
+    }
+
+}

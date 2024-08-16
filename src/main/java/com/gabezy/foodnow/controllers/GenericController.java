94cfd53389527b74a299d1b1d0dfd2383cbb84d5
gabezy <gabrielmoreira2705@gmail.com>
2024-08-16 09:41:55 -0300

    added generic controller

---
+++ src/main/java/com/gabezy/foodnow/controllers/GenericController.java
@@ -0,0 +1,12 @@
+package com.gabezy.foodnow.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GenericController<T, I, R> {
+
+    protected R repository;
+
+
+
+
+}

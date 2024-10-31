284d1a7ae049b1d5f7b77f842c5fa27b70ab95f0
gabezy <gabrielmoreira2705@gmail.com>
2024-10-31 18:41:23 -0300

    add timezone and ObjectMapper config

--- src/main/java/com/gabezy/foodnow/FoodNowApplication.java
+++ src/main/java/com/gabezy/foodnow/FoodNowApplication.java
@@ -1,14 +1,22 @@
 package com.gabezy.foodnow;
 
 import com.gabezy.foodnow.infra.repositories.GenericRepositoryImpl;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = GenericRepositoryImpl.class) // dizendo para o spring que essa classe será
                                                                           // a nova implementação do SimpleJpaRepository
 public class FoodNowApplication {
+
+    @PostConstruct
+    public void initAll() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FoodNowApplication.class, args);
---
+++ src/main/java/com/gabezy/foodnow/config/jackson/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.gabezy.foodnow.config.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
+}
